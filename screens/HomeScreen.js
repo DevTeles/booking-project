534cd6cb6c8bb1f5af9ffad8d836c05f5409853e
Rafael Teles Vital <rafael@Rafaels-MacBook-Pro.local>
2023-05-23 14:41:39 -0400

    add pressable and new lib react native date ranges

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,20 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React, { useLayoutEffect } from 'react'
-import { useNavigation } from '@react-navigation/native'
-import { Ionicons } from "@expo/vector-icons";
-
+import {
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { Ionicons, Feather } from "@expo/vector-icons";
+import Header from "../components/Header";
+import DatePicker from "react-native-date-ranges";
 
 const HomeScreen = () => {
-  const navigation = useNavigation()
+  const navigation = useNavigation();
+
   useLayoutEffect(() => {
     navigation.setOptions({
       headerShown: true,
@@ -13,25 +22,91 @@ const HomeScreen = () => {
       headerTitleStyle: {
         fontSize: 20,
         fontWeight: "bold",
-        color: "white"
+        color: "white",
       },
       headerStyle: {
         backgroundColor: "#003580",
         height: 110,
+        borderBottomColor: "transparent",
+        shadowColor: "transparent",
       },
-      headerRight: () => {
-        <Ionicons name="notifications-outline" size={24} color="white" style={{ marginRight: 12 }} />
-      }
-    })
-  },[])
+      headerRight: () => (
+        <Ionicons
+          name="notifications-outline"
+          size={24}
+          color="white"
+          style={{ marginRight: 12 }}
+        />
+      ),
+    });
+  }, []);
 
   return (
     <View>
-      <Text>HomeScreen</Text>
+      <Header />
+
+      <ScrollView>
+        <View
+          style={{
+            margin: 20,
+            borderColor: "#FFC72C",
+            borderWidth: 3,
+            borderRadius: 6,
+          }}
+        >
+          {/* Destination */}
+          <Pressable
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              gap: 10,
+              paddingHorizontal: 10,
+              borderColor: "#FFC72C",
+              borderWidth: 2,
+              paddingVertical: 15,
+            }}
+          >
+            <Feather color="black" size={24} name="search" />
+            <TextInput placeholder="Enter your destination" />
+          </Pressable>
+
+          {/* Selected Dates */}
+          <Pressable style={{
+              flexDirection: "row",
+              alignItems: "center",
+              gap: 10,
+              paddingHorizontal: 10,
+              borderColor: "#FFC72C",
+              borderWidth: 2,
+              paddingVertical: 15,
+            }}>
+            <Feather color="black" size={24} name="calendar" />
+            <DatePicker 
+              style={ { width: 350, height: 45 } }
+              customStyles = { {
+                placeholderText:{ fontSize:20 },
+                headerStyle: {
+                  backgroundColor: "#003580"
+                }
+              } } 
+              selectedBgColor="#0047AB"
+              allowFontScaling = {false} // optional
+              placeholder={'Apr 27, 2018 → Jul 10, 2018'}
+              mode={'range'}
+            />
+          </Pressable>
+
+          {/* Rooms and Guests */}
+          <Pressable></Pressable>
+
+          {/* Search Button */}
+          <Pressable></Pressable>
+        </View>
+      </ScrollView>
     </View>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
